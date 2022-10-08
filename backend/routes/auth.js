8eb3a27b73850dea9d2503c97c15da0e7d4986a7
sL0p3 <shlokjakhotia@gmail.com>
2022-10-08 14:59:33 +0530

    updated

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,19 +8,35 @@ router.post('/createuser',[
     body('name','Enter a valid name').isLength({ min: 2 }),
     body('email','Enter a valid email').isEmail(),
     body('password','Password must be atleast 5 characters').isLength({ min: 5 }),
-], (req,res)=>{ 
+], async (req,res)=>{ 
+    // If there are errors , return Bad request and the errors .
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    try {
+    // Check whether a user with this email exists already .
+    let user = await User.findOne({email : req.body.email});
+    if(user){
+        return res.status(400).json({error: "A user with this email already exists."})
+    }
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-    }).then(user => res.json(user)).catch((err)=>{
-        console.log(err);
-        res.json({error : 'Please enter a uinque email',message : err.message});
-    });
+    })
+    
+    // .then(user => res.json(user)).catch((err)=>{
+    //     console.log(err);
+    //     res.json({error : 'Please enter a uinque email',message : err.message});
+    // });
+
+    res.send(user)
+
+    } catch (error) {
+        console.error(error.message); 
+        res.status(500).send("Some error occoured");
+    }
 })
 
 module.exports = router
