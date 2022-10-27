730044ef508d0d54331c91207b23f1f0f19ed662
sL0p3 <shlokjakhotia@gmail.com>
2022-10-27 22:39:07 +0530

    note component and card

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
 import React, { useContext } from 'react'
-import NoteContext from '../context/notes/noteContext'
+import Notes from './Notes'
 
 export const Home = () => {
-  const context = useContext(NoteContext);
-  const {notes, setNotes} = context;
+
   return (
     <div>
       <div className="container my-3">
@@ -26,12 +25,7 @@ export const Home = () => {
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
-      <div className="container my-3">
-        <h2>Your Notes</h2>
-        {notes.map((note)=>{
-            return note.title
-        })}
-      </div>
+      <Notes />
    </div>
   )
 }
