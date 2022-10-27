070482b2d5d76f5de46f9fc377e47d958b1bbcb3
sL0p3 <shlokjakhotia@gmail.com>
2022-10-27 18:21:28 +0530

    useLocation updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import { Link , useLocation } from "react-router-dom";
 
 const Navbar = () => {
 
-  let location = useLocation();
-  useEffect(()=>{
-    console.log(location.pathname);
-  },[location])
+  // let location = useLocation();
+  // useEffect(()=>{
+  //   console.log(location.pathname);
+  // },[location])
 
   return (
     <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
