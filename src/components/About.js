cb390d960bbffefa6ccf404525ae8262d2d9e980
sL0p3 <shlokjakhotia@gmail.com>
2022-10-27 18:18:27 +0530

    useLocation hook added

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,10 @@
-import React,{ useContext , useEffect } from 'react'
-import NoteContext from '../context/notes/noteContext'
+import React from 'react'
 
 const About = () => {
-  const a = useContext(NoteContext)
-  useEffect(()=>{
-    a.update();
-    // eslint-disable-next-line
-  },[])
+
   return (
     <div>
-        About {a.state.name}
+        About 
     </div>
   )
 }
--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -1,24 +1,10 @@
-import React, {useState} from "react";
+import React from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name" : "notShlok",
-        "class" : "AC",
-      }
-      const [state, setState] = useState(s1);
-    
-      const update = ()=>{
-        setTimeout(() => {
-            setState({
-              "name" : "Shlok",
-              "class" : "CA"
-            })
-        }, 1000);
-      }
-    
+
     return (
-        <NoteContext.Provider value = {{state:state, update:update}}>
+        <NoteContext.Provider value = {{}}>
             {props.children}
         </NoteContext.Provider>
     )
