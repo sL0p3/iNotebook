2fd726664d2e371e0199e6db03647100f9f39879
sL0p3 <shlokjakhotia@gmail.com>
2023-04-20 23:35:08 +0530

    updated

--- backend/database .js
+++ backend/database .js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToMongo = async ()=> {
     try{ 
-    const conn = await mongoose.connect("mongodb://localhost:27017/inotebook", {
+    const conn = await mongoose.connect("https://mongodb.prashantguptaideamagix.org/api/navigation/render/main", {
         useNewUrlParser:true,
         useUnifiedTopology:true,  
     });
